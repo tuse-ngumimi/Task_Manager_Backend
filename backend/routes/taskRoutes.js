6cfa4a18f4c4be8291ebbc70d7f10b63acd2c50f
Ngumimi Tuse <tusengumimi21@gmail.com>
2026-04-06 09:15:44 +0100

    feat: add protection middleware to task routes for better security.

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router()
-const {getTask, setTask, updateTask, deleteTask} = require('../controllers/taskController')
+const {getTask, getSingleTask, setTask, updateTask, deleteTask} = require('../controllers/taskController')
 
+const { protect } = require('../middleware/authMiddleware')
 
-router.route('/').get(getTask).post(setTask)
-router.route('/:id').put(updateTask).delete(deleteTask)
+router.route('/').get(protect, getTask).post(protect, setTask)
+router.route('/:id').get(protect, getSingleTask).put(protect, updateTask).delete(protect, deleteTask)
 
 module.exports = router 
