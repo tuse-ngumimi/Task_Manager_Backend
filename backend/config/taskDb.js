3e36d5f907790f97cfafab0a4b743f96c94bba1c
Ngumimi Tuse <tusengumimi21@gmail.com>
2026-04-06 11:21:34 +0100

    fix: add check for MONGO_URI environment variable in database connection

--- backend/config/taskDb.js
+++ backend/config/taskDb.js
@@ -4,6 +4,8 @@ const connectDB = async() => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI)
 
+    if (!process.env.MONGO_URI) 
+      console.log("Environment variable MONGO_URI not found");
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error){
     console.log(error);
