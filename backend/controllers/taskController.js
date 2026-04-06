3b2d3fea771a1d81e0384202388602d11900aa91
Ngumimi Tuse <tusengumimi21@gmail.com>
2026-04-06 09:11:53 +0100

    feat: enhance retrieval of tasks with single task endpoint and improve error handling

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const Task = require('../models/taskModel')
-const User = require('../models/userModel')
+//const User = require('../models/userModel')
 
 // Retrieve all tasks
 // GET /api/tasks
@@ -10,17 +10,40 @@ const getTask = asyncHandler(async (req, res) => {
   res.status(200).json(tasks)
 })
 
+// Retrieve single task
+// Get api/tasks/:id
+// Private
+const getSingleTask = asyncHandler(async (req, res) => {
+  const task = await Task.findById(req.params.id)
+
+  if (!task) {
+    res.status(404)
+    throw new Error('Task not found')
+  }
+
+  if (task.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not authorized to view this task')
+  }
+
+  res.status(200).json(task)
+})
+
 // Create one task at a time
 // POST /api/tasks
 // Private
 const setTask = asyncHandler(async (req, res) => {
-  if (!req.body.text) {
+  const { title, description, status, dueDate } = req.body
+
+  if (!title || !description || !dueDate) {
     res.status(400)
-    throw new Error('Please add a text field')
+    throw new Error('Please add a title, description, and due date')
   }
   const task = await Task.create({
-    text: req.body.text,
     user: req.user.id,
+    title, description,
+    status: status || 'todo',
+    dueDate,
   })
   res.status(201).json(task) // 201 = Created
 })
@@ -36,15 +59,19 @@ const updateTask = asyncHandler(async (req, res) => {
     throw new Error('Task not found')
   }
 
-  if (task.use.toString() !== req.user.id){
+  if (task.user.toString() !== req.user.id){
     res.status(401)
     throw new Error('Not authorised to update task')
   }
 
   const updatedTask = await Task.findByIdAndUpdate(
     req.params.id,
-    { text: req.body.text },
-    { new: true } // returns the updated version
+    { title: req.body.title || task.title,
+      description: req.body.description || task.description,
+      status: req.body.status || task.status,
+      dueDate: req.body.dueDate || task.dueDate,
+     },
+    { new: true, runValidators: true } // returns the updated version
   )
 
   res.status(200).json(updatedTask)
@@ -71,6 +98,6 @@ const deleteTask = asyncHandler(async (req, res) => {
 })
 
 module.exports = {
-  getTask, setTask, updateTask, deleteTask
+  getTask, getSingleTask, setTask, updateTask, deleteTask
 }
 
